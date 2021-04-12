6b031c2dbee2853987c735c0f354c9bffc57e182
Luiz Garcia <luizgarcia231@gmail.com>
2021-04-12 09:16:19 -0300

    Commit
    Atualização

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
-app.set("view engine","ejs"); //view engine render HTML
+//View engine render HTML
+app.set("view engine","ejs"); 
 
+//Static
+app.use(express.static("public"));
 
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso");
+    }).catch((error) => {
+        console.log(error);
+    });
 
 
 app.get("/", (req,res) => { //rota principal
