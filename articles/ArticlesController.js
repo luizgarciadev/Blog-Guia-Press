cd820539d9ba9d86cffd9da858d93157901d6aef
Luiz Garcia <luizgarcia231@gmail.com>
2021-04-20 12:54:47 -0300

    Commit
    20/04/2021 - atualização modulo de cadastro de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req,res) => {
+//Rota para exibir artigos
+router.get("/admin/articles", (req,res) => {
     res.send("Rota de Articles");
 });
 
+//Rota criar novo artigo
 router.get("/admin/articles/new", (req,res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new",({categories: categories}));
+    })
+});
+
+//Rota para salvar artivo (formulário) no banco de dados
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category //Chave estrangeira da categoria
+    }).then(() => {
+        res.redirect("/admin/articles");
     })
 });
 
